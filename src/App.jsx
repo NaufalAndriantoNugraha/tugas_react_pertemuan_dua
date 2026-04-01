2a1c13f00872118bde47fbfa708a16763892b60a
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2026-04-01 14:02:00 +0700

    Fitur: Tambah hero section dan ubah icon pada header
    Menambahkan hero section yang menampilkan novel berjudul "The Old
    Republic: Revan", gambar yang digunakan diambil dari star wars fandom.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ export default function App() {
             href="/"
             class="d-inline-flex link-body-emphasis text-decoration-none align-items-center gap-2"
           >
-            <i class="fa-solid fa-book fa-xl" style={{ color: "#0d6efd" }}></i>
+            <i
+              class="fa-solid fa-book-open fa-xl"
+              style={{ color: "#0d6efd" }}
+            ></i>
             <span className="fw-semibold fs-5" style={{ color: "#0d6efd" }}>
               Digital Library
             </span>
@@ -15,7 +18,7 @@ export default function App() {
         </div>
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
           <li>
-            <a href="#" class="nav-link px-2">
+            <a href="/" class="nav-link px-2">
               Home
             </a>
           </li>
@@ -44,6 +47,42 @@ export default function App() {
           </button>
         </div>
       </header>
+
+      <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div class="col-10 col-sm-8 col-lg-6">
+            <img
+              src="https://static.wikia.nocookie.net/starwars/images/6/63/TOR-Revan.jpg/revision/latest?cb=20110728211622"
+              class="d-block mx-lg-auto img-fluid"
+              style={{ width: 250, height: 400, borderRadius: "0.5rem" }}
+              loading="lazy"
+            />
+          </div>
+          <div class="col-lg-6">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">
+              The Old Republic: Revan
+            </h1>
+            <p class="lead" style={{ textAlign: "justify" }}>
+              The Old Republic: Revan is the third in a series of novels based
+              on the MMORPG Star Wars: The Old Republic, and features the
+              character Revan, who was the player character in the 2003 video
+              game Star Wars: Knights of the Old Republic. Written by Drew
+              Karpyshyn, the novel was released on November 15, 2011.{" "}
+            </p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">
+                Buy the Book
+              </button>
+              <button
+                type="button"
+                class="btn btn-outline-secondary btn-lg px-4"
+              >
+                Rent the Book
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
